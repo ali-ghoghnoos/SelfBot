7a0e2bbe0b236dedcdeb01e919ce59d92833517b
telemanagerplus <jaiclaicliat@emeil.ir>
2016-03-19 09:28:52 +0330

    Fix and Added SelfBot option

--- bot/TMP_selfbot.lua
+++ bot/TMP_selfbot.lua
@@ -46,10 +46,10 @@ end
 
 function msg_valid(msg)
   -- Don't process outgoing messages
-  if msg.out then
-    print('\27[36mNot valid: msg from us\27[39m')
-    return false
-  end
+ -- if msg.out then
+ --   print('\27[36mNot valid: msg from us\27[39m')
+ --   return false
+ -- end
 
   -- Before bot was started
   if msg.date < now then
@@ -72,10 +72,10 @@ function msg_valid(msg)
     return false
   end
 
-  if msg.from.id == our_id then
-    print('\27[36mNot valid: Msg from our id\27[39m')
-    return false
-  end
+ -- if msg.from.id == our_id then
+ --   print('\27[36mNot valid: Msg from our id\27[39m')
+ --   return false
+ -- end
 
   if msg.to.type == 'encr_chat' then
     print('\27[36mNot valid: Encrypted chat\27[39m')

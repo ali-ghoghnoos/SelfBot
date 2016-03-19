34cc59e43cbe34ea53b954380bd00bb0a14ab3fb
telemanagerplus <jaiclaicliat@emeil.ir>
2016-03-19 12:15:43 +0330

    Disable Autoleave & block

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -3,12 +3,12 @@ do
 local function run(msg, matches)
 local bot_id = our_id -- your bot id
    -- like local bot_id = 1234567
-    if matches[1] == 'leave' and is_admin(msg) then
+    if matches[1] == 'leave' and is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-      send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
-      chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-      block_user("user#id"..msg.from.id,ok_cb,false)
+   --   send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
+   --   chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+   --   block_user("user#id"..msg.from.id,ok_cb,false)
     end
 end
  
